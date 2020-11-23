7e67a8e98320c5ca6a933c40bed08bb99876a558
Wisman Nur <wismannura@gmail.com>
2020-11-23 13:22:55 +0700

    test test

---
+++ pages/pkwtt.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="top" class="container">
+    <a :href="url" target="_blank" style="color: #000;">go to the link. click me.</a>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'resume',
+  data () {
+    return {
+      url: 'https://drive.google.com/file/d/1hQmvsGr_bSivcO5mlvAolymo93nN9RXK/view?usp=sharing'
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cc-contact-information {
+  background-size: cover;
+}
+</style>
